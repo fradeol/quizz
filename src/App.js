76fb3b625e2fb07743a1eaf46d5adf32c19020a7
CyrilleDeletre <cyrille.deletre@gmail.com>
2023-02-09 16:48:24 +0100

    logo + root css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import logoCrazyQuizCat from "./img/logoCrazyQuizCat.svg";
+import logoCrazyQuiz from "./img/logoCrazyQuiz.png";
+import logoTrophee from "./img/logoTrophee.png";
+import logoBrokenTrophee from "./img/logoBrokenTrophee.png";
+import logoHTMLBanner from "./img/logoHTMLBanner.svg";
+import logoCSSBanner from "./img/logoCSSBanner.svg";
+import logoJSBanner from "./img/logoJSBanner.svg";
+import logoReactBanner from "./img/logoReactBanner.svg";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+
+    // Test c'est pas la page d'accueil
+    <main>
+      <h1>Quiz HTML</h1>
+      <h2>Question 1</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Iure, inventore nihil fuga at debitis reiciendis esse voluptate aliquam sit fugiat cupiditate, 
+        dolorem numquam veritatis incidunt, obcaecati quis dolore? Accusantium, optio!
+        Illum praesentium, maiores labore, quae veniam laudantium at obcaecati facilis voluptatem 
+        itaque rem fugiat! Consequuntur, fugiat. Repellat dolorem aperiam illo quas, 
+        eum sequi hic veritatis officia debitis, cupiditate sed magnam?
+      </p>
+      <img src={logoCrazyQuiz} alt="logo du site CrazyQuiz" />
+      <img src={logoCrazyQuizCat} alt="" />
+      <img src={logoTrophee} alt="" />
+      <img src={logoBrokenTrophee} alt="" />
+      <img src={logoHTMLBanner} alt="" />
+      <img src={logoCSSBanner} alt="" />
+      <img src={logoJSBanner} alt="" />
+      <img src={logoReactBanner} alt="" />
+
+      <article className='colorHtml'>
+        <h2>HTML</h2>
+      </article>
+      <article className='colorCSS'>
+        <h2>CSS</h2>
+      </article>
+      <article className='colorJS'>
+        <h2>JS</h2>
+      </article>
+      <article className='colorReact'>
+        <h2>React</h2>
+      </article>
+
+    </main>
   );
 }
 
