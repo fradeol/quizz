d1822ac78ac6310f70aa031e5646b00a6ed9e2bd
Delecroix <d-alexis@hotmail.fr>
2023-02-14 16:28:14 +0100

    w3c

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Router from './components/Router';
+import Fetch from './components/quiz/Fetch';
 
 function App() {
   return (
     <div className="App">
+      <Fetch/>
       <Router />
     </div>
   );
