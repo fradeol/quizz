c261fd4c519df3e8ed12837fe6f73d65aa82d222
Francois De Oliveira <francois-deoliveira@hotmail.fr>
2023-02-14 13:51:31 +0100

    changes in fetch

--- src/components/quiz/Fetch.jsx
+++ src/components/quiz/Fetch.jsx
@@ -1,152 +1,55 @@
-// import { logDOM } from "@testing-library/react";
+
 import { useState, useEffect } from "react";
 
 
-export default function Fetch() {
-//   const url = fetch("http://localhost:8000/api/questions");
-
-//   let rambom;
-//   let idQuestions = [];
-//   let idReponse = [];
-//   let idPartie = [];
-//   let categorie = [];
-//   let html = [];
-
-//   url
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((listeQuestions) => {
-//       listeQuestions.forEach(function (nosQuestions) {
-//         idQuestions.push(nosQuestions.question);
-//         idReponse.push(nosQuestions.reponse1);
-//         if (nosQuestions.categorie === "HTML") {
-//           html.push(nosQuestions.question);
-//         }
-//       });
-//       while (idPartie.length < 1) {
-//         rambom = Math.floor(Math.random() * 10);
-//         // if (idPartie.indexOf(idQuestions[rambom]) === -1) {
-//         //   idPartie.push(idQuestions[rambom]);
-//         // }
-//         if (html.indexOf(idQuestions[rambom]) === -1) {
-//             idPartie.push(idQuestions[rambom]);
-//           }
-
-//       }
-//     });
-//   // console.log(idReponse);
-//   // console.log(idPartie);
-//   console.log(html);
-// }
-
-const urlNotreApi = fetch("http://localhost:8000/api/questions");
-let randomNumber;
-let allHTMLQuestions = [];
-let chosenQuestion = [];
-let reponse = [];
-
-urlNotreApi.then
-(response => 
-  {
-    return response.json();
-  }
-)
-
-.then
-(notreTableau => 
-  {
-    notreTableau.forEach
-    (function(elementDuTableau)
-      {
-        if (elementDuTableau.categorie === "HTML") 
-        {
-          allHTMLQuestions.push(elementDuTableau.question);
-        }
-        // reponse.push(elementDuTableau.id.reponse1)
-        // if (elementDuTableau.reponse1 === allHTMLQuestions.values("HTML"))
-        // {
-        //     reponse.push(elementDuTableau.reponse)
-        // }
-    // console.log(reponse)
-      }
-    );
-    // console.log(reponse)     
-    // while(chosenQuestion.length < 1) 
-    // {
-      randomNumber = Math.floor(Math.random() * 10);
-    //   if(chosenQuestion.indexOf(allHTMLQuestions[randomNumber]) === -1) 
-    //   {
-        chosenQuestion.push(allHTMLQuestions[randomNumber])
-        
-    //   }
-      
-    // }
-    console.log(chosenQuestion);
-  }
-);
-
-
-}
-    const [quizQuestion, setQuizQuestion] = useState([]);
-    const [currentIndex, setCurrentIndex] = useState(0)
+export default function Fetch({}) {
+    const [allQuestion, setAllQuestion] = useState([]);
+    const [maSelection, setMaSelection] = useState([])
+    // const [currentIndex, setCurrentIndex] = useState(0)
+    // const [questionHTML, setQuestionHTML] = useState([])
    
     useEffect(() => {
         fetch("http://localhost:8000/api/questions")
         .then(response => response.json())
         .then((data) => {
-              setQuizQuestion(data);
+            setAllQuestion(data);
+            // allQuestion.forEach((q) => { 
+            
+            let limite = 0
+            let randTableau = []
+            while ( limite < 9) {
+                randTableau.push(allQuestion[limite])
+                limite++  
+            }    
+            setMaSelection(randTableau)        
+                
+            // })
+
             }
         )
     }, [])
+    console.log(maSelection)
 
-    // quizQuestion.filter(categorie === "HTML")
-
-    let random;
+    // function random(max) {
+    //     return Math.floor(Math.random() * max);
+    // }
+    // console.log(random(5));
+    // let random;
     // let idQuestions = [];
-    let idPartie = [];
-
-    // }).then(listeQuestions => {
-    //         listeQuestions.forEach(function(nosQuestion){
-    //         if (nosQuestion.categorie === "HTML") {
-    //             idQuestions.push(nosQuestion.question);
-    //         }
-            
-            // idReponse.push(question.reponse1);
-            // categorie.push(question.categorie)
-        //     });
-        //     while(idPartie.length < 4) {
-        //         random = Math.floor(Math.random() * 10);
-        //         if(idPartie.indexOf(idQuestions[random]) === -1) {
-        //             idPartie.push(idQuestions[random])
-        //         }
-        //     }
-        // });
-        // console.log(idReponse);
-        // console.log(idPartie);
-        // console.log(categorie);  
-        
-        const generateQuestion = () => {
-            quizQuestion.map((q) => {
-                if (q.categorie === "HTML") {
-                    while (idPartie.length < 4) {
-                        random = Math.floor(Math.random() * 10);
-                        if(idPartie.indexOf(quizQuestion[random]) === -1) {
-                            idPartie.push(quizQuestion[random])
-                        }
-                        
-                    }
-                    return(
-                            setQuizQuestion(idPartie)
-                        )
-                }
-            })
-        }
+    // let idPartie = [];
 
     return (
         <div>
-            {generateQuestion()}
-            {console.log(quizQuestion)}
+            
+            <ul>
+                {maSelection.map((q, i) => {
+                    return (
+                    <li key={i}>{q}</li>
+                    )
+                })}
+            </ul> 
+            
+            {/* {console.log(questionHTML)} */}
             {/* {quizQuestion.map((q)=>{
                 if(q.categorie==="HTML")
                 
