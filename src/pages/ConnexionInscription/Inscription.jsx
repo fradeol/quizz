c6eb0eea32b381b0b3129f9f4d6b4938c087ebd6
CyrilleDeletre <cyrille.deletre@gmail.com>
2023-02-15 15:23:53 +0100

    changement css

--- src/pages/ConnexionInscription/Inscription.jsx
+++ src/pages/ConnexionInscription/Inscription.jsx
@@ -1,69 +1,70 @@
 import "./ConnexionInscription.css";
-import Header from "../../components/header/Header";
 import { UserContext } from "../../context/UserContext";
-import { useContext } from "react";
-import { RxCross1} from 'react-icons/rx';
-import logoCrazyQuizCat from "../../img/logoCrazyQuizCat.svg";
+import { useContext, useRef, useState } from "react";
+import { RxCross1 } from "react-icons/rx";
 
 export default function Inscription() {
-
   const { modalState, toggleModals } = useContext(UserContext);
 
   return (
-    
-<>
-{modalState.signUpModal && (
-    <div className="conteneurModal">
-      <div className="centre">
-        <div className="logoCat">
-        <img src={logoCrazyQuizCat} alt="" />
-        </div>
-      <button className="close" onClick={() => toggleModals("close")}> <RxCross1/></button>
-      </div>
-      <form action="">
-        <fieldset>
-          <legend>Inscription</legend>
-          {/* <input 
-          type={"text"} 
-          placeholder="pseudo" 
-          name="pseudo" 
-          id="pseudo"
-          required/> */}
+    <>
+      {modalState.signUpModal && (
+        <div>
+          <div 
+          className="overlay"
+          onClick={() => toggleModals("close")}></div>
+          <div className="conteneurModal">
+            <div className="centre">
+              <button className="close" onClick={() => toggleModals("close")}>
+                {" "}
+                <RxCross1 />
+              </button>
+            </div>
+            <form action="">
+              <fieldset>
+                <legend>Inscription</legend>
+                {/* <input
+            type={"text"}
+            placeholder="pseudo"
+            name="pseudo"
+            id="pseudo"
+            required/> */}
+                <input
+// AJOUTER UNE REF                
+                  type={"email"}
+                  placeholder="adresse@mail.fr"
+                  name="email"
+                  id="signUpEmail"
+                  required
+                />
+                <input
+// AJOUTER UNE REF                
+                  type={"password"}
+                  placeholder="mot de passe"
+                  name="password"
+                  id="password"
+                  required
+                />
 
-          <input 
-          type={"email"} 
-          placeholder="adresse@mail.fr" 
-          name="email" id="signUpEmail"
-          required/>
-
-          <input 
-          type={"password"} 
-          placeholder="mot de passe" 
-          name="password" 
-          id="password"
-          required/>
-
-<input 
-          type={"password"} 
-          placeholder="repeter mot de passe" 
-          name="repeatPassword" 
-          id="repeatPassword"
-          required/>
-        <p className="red">Validation</p>
-          <div className="checkbox">
-            <label htmlFor="">
-              {" "}
-              Se souvenir de moi
-            </label>
-            <input type={"checkbox"} />
-            
+                <input
+// AJOUTER UNE REF                
+                  type={"password"}
+                  placeholder="repeter mot de passe"
+                  name="repeatPassword"
+                  id="repeatPassword"
+                  required
+                />
+                <p className="red">Validation</p>
+                <div className="checkbox">
+                  <label htmlFor=""> Se souvenir de moi</label>
+                  <input type={"checkbox"} />
+                </div>
+                <button type="submit">S'inscrire</button>
+              </fieldset>
+            </form>
           </div>
-
-          <button type="submit">S'inscrire</button>
-        </fieldset>
-      </form>
-    </div>
-    )}
-    </>  
+        </div>
+      )}
+    </>
   );
 }
