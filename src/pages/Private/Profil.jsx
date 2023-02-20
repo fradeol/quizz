ee6e10d7d5cf697e425a04adb39e97965a3350c7
Francois De Oliveira <francois-deoliveira@hotmail.fr>
2023-02-20 16:58:22 +0100

    profile page part1

--- src/pages/Private/Profil.jsx
+++ src/pages/Private/Profil.jsx
@@ -1,12 +1,33 @@
 import './Profil.css'
+import logoCrazyQuizCat from "../../img/logoCrazyQuizCat.svg";
+import logoTrophee from "../../img/logoTrophee-removebg-preview.png";
+import { Link } from "react-router-dom";
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
 
 export default function Profil() {
 
+    const {currentUser} = useContext(UserContext)
+    console.log(currentUser);
     return (
-        <div className='profil'>
-            <div className="courbe">
-
+        <div className="profil">
+            <div className='score'>
+                <img src={logoTrophee} alt="" />
+                <p>1850 pts</p>
             </div>
+            <img src={logoCrazyQuizCat} alt="" />
+            <div className='courbe'></div>
+            <div className="donneesUser">
+                {/* <p>{currentUser.displayName}</p> */}
+                <p>{currentUser.email}</p>
+                <p>Mot de Passe</p>
+            </div>
+            
+            <div className='connectedBtn'>
+                <Link to='/Private/Home' >Retour</Link>
+            </div>
+            
         </div>
+        
     )
 }
